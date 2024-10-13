c437c8d2fbaaf4806d57bbf34047ea91e6008bd0
Ji, jeonguk <dkdpawjddnr@naver.com>
2024-10-13 19:54:20 +0900

    "Modle 추가 - v3"

--- servlet/src/main/java/hello/servlet/web/frontcontroller/MyView.java
+++ servlet/src/main/java/hello/servlet/web/frontcontroller/MyView.java
@@ -1,22 +1,28 @@
 package hello.servlet.web.frontcontroller;
-
 import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
-
+import java.util.Map;
 public class MyView {
     private String viewPath;
-
     public MyView(String viewPath) {
         this.viewPath = viewPath;
     }
-
-    //View를 만드는 행위를 '랜더링 한다.' 라고 함.
-    public void render(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
+    public void render(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         RequestDispatcher dispatcher = request.getRequestDispatcher(viewPath);
         dispatcher.forward(request, response);
+    }
+    public void render(Map<String, Object> model, HttpServletRequest request,
+                       HttpServletResponse response) throws ServletException, IOException {
+        modelToRequestAttribute(model, request);
+        RequestDispatcher dispatcher = request.getRequestDispatcher(viewPath);
+        dispatcher.forward(request, response);
+    }
+    private void modelToRequestAttribute(Map<String, Object> model,
+                                         HttpServletRequest request) {
+        model.forEach((key, value) -> request.setAttribute(key, value));
     }
 }
